ce76d7a8c02a8c3b08a81e06601722932898fc37
新井晟矢 <Seiya@S-Arai-Seya.local>
2017-06-24 20:13:16 +0900

    初めて

--- camera/ViewController.swift
+++ camera/ViewController.swift
@@ -8,7 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    
+    
+    @IBOutlet var cameraImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +24,35 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func openAlbum() {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            let picker = UIImagePickerController()
+            picker.sourceType = .photoLibrary
+            picker.delegate = self
+            
+            picker.allowsEditing = true
+            
+            present(picker, animated: true, completion: nil)
+            
+    }
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        
+        cameraImageView.image = image
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+        
+        
 }
+
+
+        
+
+
+
+
 
